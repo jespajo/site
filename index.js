de90840154e901d752dfed061592610d47d077f6
Jesse <jesseparisjourdan@gmail.com>
2018-12-26 18:39:07 +1000

    continued app creation

--- index.js
+++ index.js
@@ -1,41 +1,52 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const request = require('request-promise');
 
 const PORT = process.env.PORT || 5000;
 
 
 
 const d = './public/d/';
-let dJSON = [];
+let json = [];
+
 fs.readdir(d, (err, files) => {
   files.forEach(file => {
     if (file.endsWith(".html")) {
+      fs.readFile(d + file, 'utf8', function(err, html) {
+          if (err) throw err;
 
-      const url = "https://jespajo.com/post/" + file;
-      request(url)
-        .then(function(html){
-          /*
-          const htmlElem = document.createElement('html');
-          const frag = document.createDocumentFragment();
-          htmlElem.innerHTML = html;
-          frag.appendChild(htmlElem);
-          const title = frag.firstChild.getElementsByTagName('title')[0].textContent
-            || frag.firstChild.getElementsByTagName('title')[0].innerText;
+          // find title
+          const titlePos = [
+            html.search("<title>") + 7,
+            html.search("</title>") ];
+          const title = html.substring(titlePos[0], titlePos[1]);
 
-          console.log(title);
-          */
-        })
-        .catch(function(err){
-          //handle error
-        });
+          // find date
+          let date;
+          const datePos = html.search('<meta name="date"');
 
-
-      // console.log(file);
+          // if no date <meta> just use title
+          if (datePos === -1) {
+            date = file.substring(0, 8); }
+          else {
+            date = html.substring(datePos + 27, datePos + 35); }
+          date = date.substring(0, 4) +
+            "-" + date.substring(4, 6) +
+            "-" + date.substring(6, 8);
+          date = new Date(date).toISOString();
+          
+          // push obj to json
+          const dObj = {
+            date: date,
+            title: title,
+            url: d + file };
+          json.push(dObj);
+      });
     }
   });
 });
+
+
 
 /*
 express()
